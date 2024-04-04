0b95ca79de606eeb05115c4622ad26211e118dd4
khadijabensaad <khadijabensaad4@gmail.com>
2024-04-04 11:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // const { addproduct, updateproduct , getproducts ,getproductById , deleteProduct , sortProductPrix , sortProductQuantite } = require("./services");
-const { addproduct,  getproducts ,sortProductPrix , sortProductQuantite } = require("./services");
+const {
+  addproduct,
+  getproducts,
+  sortProductPrix,
+  sortProductQuantite,
+} = require("./services");
 
 let products = [];
-  
+
 // let product = {
 //     libelle : "ballon",
 //     prix : 500,
@@ -18,66 +23,63 @@ addproduct(
 );
 //...
 addproduct(
-    {
-      libelle: "avion",
-      prix: 5000,
-      quantite: 1,
-    },
-    products
-  );
+  {
+    libelle: "avion",
+    prix: 5000,
+    quantite: 1,
+  },
+  products
+);
 
 addproduct(
-    {
-      libelle: "voiture",
-      prix: 100,
-      quantite: 50,
-    },
-    products
-  );
+  {
+    libelle: "voiture",
+    prix: 100,
+    quantite: 50,
+  },
+  products
+);
 
-  addproduct(
-    {
-      libelle: "ballon",
-      prix: 500,
-      quantite: 5,
-    },
-    products
-  );
+addproduct(
+  {
+    libelle: "ballon",
+    prix: 500,
+    quantite: 5,
+  },
+  products
+);
 
-  addproduct(    
-    {
-      libelle: "pc",
-      prix: 15,
-      quantite: 5,
-    },
-    products
-  );
-  console.log("original")
+addproduct(
+  {
+    libelle: "pc",
+    prix: 15,
+    quantite: 5,
+  },
+  products
+);
+console.log("original");
 getproducts(products);
 // getproductById(2,products)
 
-// products = deleteProduct("ballon",products) // liste 
+// products = deleteProduct("ballon",products) // liste
 
 // let p = { libelle: "t", prix: 5, quantite: 50 };
 
-
- //updateproduct(0,p,products);
-
+//updateproduct(0,p,products);
 
 // getproducts(products);
 
-products = sortProductPrix(products) // liste 
+products = sortProductPrix(products); // liste
 
-// products = sortProduct("prix",products) // liste 
+// products = sortProduct("prix",products) // liste
 
-// products = sortProduct("libelle",products) // liste 
+// products = sortProduct("libelle",products) // liste
 
+getproducts("ba3ed", products);
 
-getproducts("ba3ed",products);
+products = sortProductQuantite(products); // liste
 
-products = sortProductQuantite(products) // liste 
-
-getproducts("ba3ed",products);
+getproducts("ba3ed", products);
 
 /*
 console.log("sortProductPrix",sortProductPrix(products)) 
